736aff937af4ba85b9dc48d2b6c0230f5955178d
msamiran94@outlook.com <SidSam1024$>
2020-05-23 19:22:21 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import sys
+# Replace below path with the absolute path of the \
+# chromedriver in your computer
+driver = webdriver.Chrome(r'c:\users\hp\chromedriver')
+
